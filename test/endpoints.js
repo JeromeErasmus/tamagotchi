b5bbddce0f5509a4d9cfde66db2fda0b44fcfc61
Jerome Erasmus <jeromeerasmus@Jeromes-MBP.lan>
2018-07-05 17:53:24 +1000

    updated tests

--- test/endpoints.js
+++ test/endpoints.js
@@ -1,5 +1,44 @@
-describe("test the status endpoint", () => {
-    it("converts the basic colors", () => {
+const request = require('request');
+const chai = require('chai');
+const expect = chai.expect;
+let methods = require('../src/server/app.js');
+const serverUrl = 'http://localhost:8080';
+let server = null;
 
+describe('test the server endpoints', () => {
+
+  // set up
+  let url = serverUrl + "/api/getstatus";
+  before(() => {
+    server = methods.app.listen(8080, () => methods.run());
+  });
+  
+  describe('test GET /api/getstatus', () => {
+    it('returns status 200', () => {
+      let url = serverUrl + "/api/getstatus";
+      request(url, (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+      });
     });
+
+    it('expect status properties to be above 0', () => {
+      request(url, (error, response, body) => {
+        var jsonObj = JSON.parse(body);
+        // check props
+        expect(jsonObj).to.have.property('isAlive').to.equal(true);
+        expect(jsonObj.stats).to.be.a('object');
+        
+        Object.keys(jsonObj.stats).forEach(element => {
+          expect(jsonObj.stats[element].to.be.greaterThan(0));
+        });
+
+      });
+    });
+  });
+
+  // tear down. 
+  after(() => {
+    // Shut down the server 
+    server.close();
+  })
 });
